4e6b315e61f47faccdb36270f2d13e5d4a023eb0
rishikant <charorark@gmail.com>
2019-03-25 12:42:03 +0530

    rihikant

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -5,8 +5,7 @@ import { HttpLink } from "apollo-link-http";
 export const client = new ApolloClient({
   link: new HttpLink({
     credentials: "include",
-    // uri: process.env.REACT_APP_SERVER_URL
-    uri: "https://polar-journey-65278.herokuapp.com"
+    uri: process.env.REACT_APP_SERVER_URL
   }),
   cache: new InMemoryCache()
 });
